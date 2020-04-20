7723e0e82ae18b0e965b0cc027ea877c514f09d1
Ivan Okhrimenko <vanya.okhrimenko@gmail.com>
2020-04-20 15:33:44 +0800

    added build-elements

---
+++ build-elements.js
@@ -0,0 +1,15 @@
+// build-elements.js
+const fs = require('fs-extra');
+const concat = require('concat');
+const widgetName = 'widget-name';
+
+(async function build() {
+    const files = [
+        './dist/widget/scripts.js',
+        './dist/widget/runtime-es2015.js',
+        './dist/widget/main-es2015.js',
+    ];
+    await fs.ensureDir('elements');
+    console.log(files)
+    await concat(files, `elements/${widgetName}.js`);
+})().catch(err => console.debug(err));
